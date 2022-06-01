ae9030b39fc688d9e6631f9eb8315b331a48b560
Tahmin Kabulzad <tahmin@Tahmins-MacBook-Air.local>
2022-06-01 17:16:05 -0400

    4th commit

---
+++ src/github/gitAndGithub.java
@@ -0,0 +1,15 @@
+package github;
+
+public class gitAndGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			System.out.print("today we learn github");
+		
+		
+		
+	}
+
+}
